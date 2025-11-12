119a659ce73a046157f962b8b78154f5949c0413
Flpnk <daniilfilipenko7504@gmail.com>
2025-11-12 23:09:50 +0300

    Initial commit bonus miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 5500;
+        int x = 20;
+
+        int miles = amount / x;
+
+        System.out.println(miles);
+    }
+}
