3697821a61b16e28174f78ce5020cdb51fdfebfe
Flpnk <daniilfilipenko7504@gmail.com>
2025-11-17 22:59:52 +0300

    Update BonusMilesService v2 JavaClass

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class BonusMilesService {
+    public int calculate (int price) {
+        int rubMile = 20;
+        int miles = price / rubMile;
+        return miles;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 5500;
-        int rubMile = 20;
-
-        int miles = amount / rubMile;
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
         System.out.println(miles);
     }
 }
